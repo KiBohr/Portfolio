f6753afe53acfcf24829961e36a1a70bb8beeff6
KiBohr <katharinabohr@web.de>
2025-07-11 17:10:48 +0200

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-import { createBrowserRouter, createRoutesFromElements, Route, Router, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
 import Layout from "./layout/Layout"
 import Home from "./page/home/Home"
+import AboutMe from "./page/aboutMe/AboutMe"
+import SkillsTech from "./page/skillsTech/SkillsTech"
+import ExpEdu from "./page/expEdu/ExpEdu"
+import Projects from "./page/projects/Projects"
+import Contact from "./page/contact/contact"
 
 function App() {
 
@@ -8,6 +13,11 @@ function App() {
     createRoutesFromElements(
       <Route path="/" element={<Layout/>}>
         <Route index element ={<Home/>}/>
+        <Route path="aboutMe" element={<AboutMe/>}/>
+        <Route path="skills&tech" element={<SkillsTech/>}/>
+        <Route path="exp&edu" element={<ExpEdu/>}/>
+        <Route path="projects" element={<Projects/>}/>
+        <Route path="contact" element={<Contact/>}/>
 
       </Route>
     )
