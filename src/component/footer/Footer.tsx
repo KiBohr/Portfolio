1244e44235fff0b8bcd6532f1882598af69064e0
KiBohr <katharinabohr@web.de>
2025-07-15 15:13:29 +0200

    deleted footer content for now

--- src/component/footer/Footer.tsx
+++ src/component/footer/Footer.tsx
@@ -1,17 +1,8 @@
 const Footer = () => {
     return ( 
-        <div className="mt-auto flex items-center justify-end gap-4 m-3">
-              {/*  component for dark light mode */}
-            <img 
-            className="hidden cursor-pointer hover:scale-110 transition-all duration-300 md:block md:h-8 lg:h-10" 
-            src="/src/assets/img/lightMode.svg" 
-            alt="doodle of a moon, dark/light mode" />
-
-            {/*  component for language change */}
-            <img 
-            className="hidden cursor-pointer hover:scale-110 transition-all duration-300 md:block md:h-8 lg:h-10" 
-            src="/src/assets/img/language.svg" 
-            alt="DE, button to change languages" />
+        <div className="mt-10 flex items-center justify-end gap-4">
+             
+          
         </div>
       
      );
