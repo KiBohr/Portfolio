f5638203e4d373d78c5e1afd2376f843d1b0b5d9
KiBohr <katharinabohr@web.de>
2025-07-15 15:13:58 +0200

    changed header back

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -2,7 +2,8 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
     return ( 
-        <header className="flex items-center justify-between gap-4 m-3 mb-5">
+supercode
+        <header className="flex items-center justify-between gap-4 mx-2 mb-10 md:mx-5 md:mb-20">
             <Link 
             to="/">
             <img 
@@ -15,13 +16,13 @@ const Header = () => {
             <div className="flex items-center gap-2 justify-center">
                  {/*  component for dark light mode */}
             <img 
-            className="cursor-pointer h-5 block md:hidden hover:scale-110 transition-all duration-300" 
+            className="cursor-pointer h-5 md:h-8 lg:h-10  hover:scale-110 transition-all duration-300" 
             src="/src/assets/img/lightMode.svg" 
             alt="doodle of a moon, dark/light mode" />
 
             {/*  component for language change */}
             <img 
-            className="cursor-pointer h-5 block md:hidden hover:scale-110 transition-all duration-300" 
+            className="cursor-pointer h-5 md:h-8 lg:h-10 hover:scale-110 transition-all duration-300" 
             src="/src/assets/img/language.svg" 
             alt="DE, button to change languages" />
             </div>
