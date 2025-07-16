eb90fd0148965c1d5787e7ec343fde8cd76bc4fc
KiBohr <katharinabohr@web.de>
2025-07-16 23:28:43 +0200

    small changes to open tab extra

--- src/component/linkButton/LinkButton.tsx
+++ src/component/linkButton/LinkButton.tsx
@@ -7,7 +7,10 @@ interface LinkButtonProps {
 
 const LinkButton = ({link, src, alt, styling} : LinkButtonProps) => {
     return ( 
-        <a 
+        <a
+        // opens in new tab
+        target="_blank"
+        rel="noopener noreferrer"
         href={link}
         className="cursor-pointer hover:scale-110 transition-all duration-300">
                 <img
