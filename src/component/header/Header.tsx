635bf796dcf0195adf65ce9bb72ba871cedb8050
KiBohr <katharinabohr@web.de>
2025-07-16 15:30:06 +0200

    added animation to the header

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,27 +1,37 @@
 import { Link } from "react-router-dom";
+import { motion } from "motion/react"
+
 
 const Header = () => {
     return ( 
         <header className="flex items-center justify-between gap-4 mx-2 mb-5  md:mx-5 md:mb-10">
             <Link 
             to="/">
-            <img 
-            className="h-8 md:h-10 lg:h-15 hover:scale-110 transition-all duration-300"
-            src="/src/assets/img/logo1.svg" 
-            alt="logo KB" />
+                <motion.img
+                    src="/src/assets/img/logo1.svg"
+                    alt="logo KB"
+                    className="h-10"
+                    initial={{ opacity: 0, scale:0 }}
+                    animate={{ opacity: 1, scale:1}}
+                    transition={{ duration: 1, type: "spring" }}
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.96 }}
+                    >
+                </motion.img>
             </Link>
+
 
 
             <div className="flex items-center gap-2 justify-center">
                  {/*  component for dark light mode */}
             <img 
-            className="cursor-pointer h-5 md:h-8 lg:h-10  hover:scale-110 transition-all duration-300" 
+            className="cursor-pointer h-5 md:h-6 lg:h-7 hover:scale-110 transition-all duration-300" 
             src="/src/assets/img/lightMode.svg" 
             alt="doodle of a moon, dark/light mode" />
 
             {/*  component for language change */}
             <img 
-            className="cursor-pointer h-5 md:h-8 lg:h-10 hover:scale-110 transition-all duration-300" 
+            className="cursor-pointer h-5 md:h-6 lg:h-7 hover:scale-110 transition-all duration-300" 
             src="/src/assets/img/language.svg" 
             alt="DE, button to change languages" />
             </div>
