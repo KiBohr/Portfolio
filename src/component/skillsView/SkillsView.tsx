0ec43cfe02e23b58db9f5d08b11cf23e3b2f1513
KiBohr <katharinabohr@web.de>
2025-07-16 15:29:49 +0200

    added skills + used motion the first time

---
+++ src/component/skillsView/SkillsView.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { motion, AnimatePresence } from "motion/react";
+
+// Beispiel-Array mit Images für Skills
+const initialSkills = [
+    { id: 1, src: "/src/assets/img/HTML5.svg", name: "HTML" },
+    { id: 2, src: "/src/assets/img/JavaScript.svg", name: "JavaScript" },
+    { id: 3, src: "/src/assets/img/TypeScript.svg", name: "Typescript" },
+    { id: 4, src: "/src/assets/img/React.svg", name: "React" },
+    { id: 5, src: "/src/assets/img/Next.js.svg", name: "next.js" },
+    { id: 6, src: "/src/assets/img/motion.svg", name: "motion" },
+    { id: 7, src: "/src/assets/img/Tailwind.svg", name: "Tailwind" },
+    { id: 8, src: "/src/assets/img/css3-plain.svg", name: "CSS" },
+    { id: 9, src: "/src/assets/img/sass.svg", name: "Sass" },
+    { id: 10, src: "/src/assets/img/nodejs.svg", name: "node.js" },
+    { id: 11, src: "/src/assets/img/Github.svg", name: "GitHub" },
+    { id: 12, src: "/src/assets/img/git.svg", name: "git" },
+    { id: 13, src: "/src/assets/img/Supabase.svg", name: "Supabase" },
+    { id: 14, src: "/src/assets/img/Figmaa.svg", name: "Figma" },
+];
+
+export default function SkillsView() {
+  const [skills] = useState(initialSkills);
+
+
+  return (
+    <div className="max-w-xs mx-auto mt-12 md:mt-20 md:max-w-lg lg:max-w-2xl mb-5">
+      <ul className="flex flex-wrap justify-evenly  gap-5">
+        <AnimatePresence>
+          {skills.map((skill) => (
+            <motion.li
+              key={skill.id}
+              className="flex justify-evenly items-center gap-3 bg-white/30 rounded-xl shadow-sm px-3 py-3 md:px-5 cursor-grab"
+              initial={{ opacity: 0, scale: 0.7 }}
+              animate={{ opacity: 1, scale: 1 }}
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 1.1 }}
+              drag // erlaubt Dragging in alle Richtungen (alternativ: drag="y" oder drag="x")
+              whileDrag={{color:"#C3CE72"}}
+              dragElastic={0.1}
+              dragSnapToOrigin // springt zurück an den Startpunkt
+              layout
+            >
+              <motion.img
+                src={skill.src}
+                alt={skill.name}
+                className="w-8 h-8"
+                layoutId={skill.id + "-img"}
+                draggable={false}
+              />
+              <span className="font-semibold text-sm">{skill.name}</span>
+            </motion.li>
+          ))}
+        </AnimatePresence>
+      </ul>
+    </div>
+  );
+}
--- src/page/skillsTech/SkillsTech.tsx
+++ src/page/skillsTech/SkillsTech.tsx
@@ -1,6 +1,17 @@
+import SkillsView from "../../component/skillsView/SkillsView";
+
 const SkillsTech = () => {
     return ( 
-        <h1>Skills & Techstack</h1>
+        <section className="flex flex-col items-center py-5">
+            <img 
+                src="/src/assets/img/skills&tech.svg" 
+                 alt="skills&tech"
+                className="h-14 md:h-15" 
+                />
+             <SkillsView/>
+             <p className="text-[0.6rem]">drag skills if you like :)</p>
+        </section>
+       
      );
 }
  
