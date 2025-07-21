766576b4d5a130aa9e4e82b0cfa2beb0c65be778
KiBohr <katharinabohr@web.de>
2025-07-21 21:37:01 +0200

    implemtent new features to the exp&Edu page

--- src/page/expEdu/ExpEdu.tsx
+++ src/page/expEdu/ExpEdu.tsx
@@ -15,16 +15,19 @@ const ExpEdu = () => {
             <motion.img
                 src="/src/assets/img/Ex&Ed.svg" 
                 alt="education and experience"
-                className="h-20 mb-10 md:mb-15"
+                className="h-20 md:h-25 mb-10 md:mb-20 "
             ></motion.img>
 
-            <div className="flex flex-col  gap-5 items-center ">
+        <div className="flex flex-col md:flex-row items-center gap-5 justify-center md:gap-1 md:justify-between">
+
+            {/* education */}
+            <div className="flex flex-col gap-5 items-center">
                 <CvInfo 
                     date="11.24 - 05.25" 
                     title="Umschulung Web-Development" 
-                    compmanyLink="https://www.super-code.de/" 
+                    companyLink="https://www.super-code.de/" 
                     company="Supercode" 
-                    description="Ich habe im Mai 25 meine Umschulung als Web Developerin erfolgreich abschließen können. Dabei lag der Fokus auf Typescript und React." 
+                    description="Ich habe im Mai 25 meine Umschulung als Web Developer*in erfolgreich abschließen können. Dabei lag der Fokus auf Typescript und React." 
                     document="/src/assets/pdfs/Katharina-Bohr_Certificate.pdf" 
                     documentTitle="Zertifikat" 
                 />
@@ -32,13 +35,30 @@ const ExpEdu = () => {
                 <CvInfo
                     date="2016 - 2020"
                     title="Studium Logopädie"
-                    compmanyLink="https://www.hs-fresenius.de/?crmid=aCbGbAJcDaAaAaAaA&accountId=4081124337&campaignId=17428330071&adgroupId&creativeId&keywordId&gad_source=5&gad_campaignid=17428332954&gclid=EAIaIQobChMIhae977bGjgMVAf15BB3VWBeEEAAYASAAEgIKJPD_BwE"
+                    companyLink="https://www.hs-fresenius.de/?crmid=aCbGbAJcDaAaAaAaA&accountId=4081124337&campaignId=17428330071&adgroupId&creativeId&keywordId&gad_source=5&gad_campaignid=17428332954&gclid=EAIaIQobChMIhae977bGjgMVAf15BB3VWBeEEAAYASAAEgIKJPD_BwE"
                     company="Hochschule Fresenius"
                     description="2020 habe ich meinen Bachelor of Science als akademische Sprachtherapeutin abgeschlossen."
                 />
             </div>
 
-            <p></p>
+            {/* visual divider */}
+            <hr className="md:rotate-90 border-red/70 w-1/2 md:w-1/4 m-5 " />
+
+            {/* experience */}
+            <div className="flex flex-col gap-5 items-center ">
+            <CvInfo
+                date="2020-2023"
+                title="Arbeit als Logopädin"
+                description="Ich habe nach meinem Studium durch das Deutschlandstipendium zu meinem neuen Arbeitsplatz gefunden und als Logopä*in für knapp drei Jahre in einer Praxis in Hamburg gearbeitet."/>
+
+            <CvInfo
+                date="ab 01.09.2025"
+                title ="Hier könnte Ihr Unternehmensname stehen"
+                description="Kommen sie gerne auf mich zu! Schauen Sie sich gerne auch meine Projekte an."
+                hovered={true}
+                />
+            </div>
+        </div>    
        </motion.section>
      );
 }
