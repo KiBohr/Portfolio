ed44a7085de66ea24fa33e87840017606b441964
KiBohr <katharinabohr@web.de>
2025-07-29 15:45:33 +0200

    changes for color toggle

--- src/page/aboutMe/AboutMe.tsx
+++ src/page/aboutMe/AboutMe.tsx
@@ -26,17 +26,17 @@ const AboutMe = () => {
              >
            
                 <div 
-                    className="bg-white/20 rounded-xl p-4 text-[0.7rem] flex flex-col items-center text-center md:text-start md:text-[0.8rem] lg:text-base gap-2 text-red/80 md:w-1/2">
-                        <p className="text-red text-start">Moin,
+                    className="bg-white/20 rounded-xl p-4 text-[0.7rem] flex flex-col items-center text-center md:text-start md:text-[0.8rem] lg:text-base gap-2  md:w-1/2">
+                        <p className=" text-start">Moin,
                             ich freue mich über den Besuch auf meiner Seite :)
                         </p>
                         <p>
-                            Mein Name ist Katharina, ich bin fast 30 Jahre alt und habe dieses Jahr meine Umschulung als <span className="text-red">Webdeveloper*in</span> abgeschlossen. 
+                            Mein Name ist Katharina, ich bin fast 30 Jahre alt und habe dieses Jahr meine Umschulung als <span >Webdeveloper*in</span> abgeschlossen. 
                         </p>
                         <p>
-                        Ich bin davon überzeugt, dass jede Fähigkeit dabei unterstützen kann Probleme auf <span className="text-red">individuelle</span>  Art und Weise zu lösen. Meine Erfahrung als Logopäd*in hat im ersten Moment wenig mit dem Feld des Entwickelns zu tun, hilft mir aber dabei
+                        Ich bin davon überzeugt, dass jede Fähigkeit dabei unterstützen kann Probleme auf <span >individuelle</span>  Art und Weise zu lösen. Meine Erfahrung als Logopäd*in hat im ersten Moment wenig mit dem Feld des Entwickelns zu tun, hilft mir aber dabei
                         </p>
-                        <ul className="list-disc text-red flex flex-col items-start">
+                        <ul className="list-disc flex flex-col items-start">
                             <li>mich produktiv selbstständig zu organisieren</li>
                             <li>effizient und zielgerichtet in einem interdisziplinären Team zu arbeiten</li>
                             <li>und kreativ immer unterschiedliche Probleme angepasst lösen zu können.</li>
