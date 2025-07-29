9dbae3b2a10917b8ba4cc55ccb150262ca05d6a7
KiBohr <katharinabohr@web.de>
2025-07-29 15:45:21 +0200

    changes for icon

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -5,7 +5,7 @@ const Home = () => {
     return ( 
 
         <motion.section
-        className="flex flex-col items-center justify-center gap-10 md:flex-row md:text-6xl md:gap-15 lg:text-8xl lg:gap-20 max-h-screen"
+        className="flex flex-col items-center justify-center gap-10 mt-10 md:flex-row md:text-6xl md:gap-15 md:mt-20 lg:mt-10 lg:text-8xl lg:gap-25 h-full w-full"
         initial={{opacity: 0}}
         animate={{opacity:1}}
         exit={{opacity:0}}
@@ -14,11 +14,10 @@ const Home = () => {
                 ease: easeInOut,
         }}
         >
-                <img
-            className="h-[40vh] rounded-4xl md:h-[60vh] lg:h-[70vh]" 
-            src="/img/platzhalterBild.svg" 
-            alt="abstract" 
-            />
+               <motion.img
+               src="/img/logo1.svg"
+               alt="an image of a K and a B melted together, the logo of this website"
+               className="h-60  md:h-90 "/>
             
             <div
             className="break-words text-center uppercase flex flex-col gap-3 items-center justify-center text-4xl md:items-start md:text-5xl md:gap-6 md:text-start">   
@@ -26,35 +25,35 @@ const Home = () => {
                     <img 
                         src="/img/aboutMe.svg" 
                         alt="about me"
-                        className="h-12 md:h-15" />
+                        className="h-10 md:h-16 lg:h-20" />
                 </Link>
                         
                 <Link className="cursor-pointer hover:scale-110 transition-all duration-300"to="skills&tech">
                 <img 
                         src="/img/skills&tech.svg" 
                         alt="skills&tech"
-                        className="h-12 md:h-15" />
+                        className="h-12 md:h-20 lg:h-24" />
                 </Link>
 
                 <Link className="cursor-pointer hover:scale-110 transition-all duration-300"to="exp&edu">
                 <img 
                         src="/img/Ex&Ed.svg" 
                         alt="about me"
-                        className="h-19 md:h-25" />
+                        className="h-20 md:h-28 lg:h-32" />
                 </Link>
 
                 <Link className="cursor-pointer hover:scale-110 transition-all duration-300"to="projects">
                 <img 
                         src="/img/projects.svg" 
                         alt="about me"
-                        className="h-12 md:h-15" />
+                        className="h-12 md:h-20 lg:h-24" />
                 </Link>
 
                 <Link className="cursor-pointer hover:scale-110 transition-all duration-300"to="contact">
                 <img 
                         src="/img/Contact.svg" 
                         alt="about me"
-                        className="h-12 md:h-15" />
+                        className="h-12 md:h-20 lg:h-24" />
                 </Link>
             </div>
         </motion.section>
