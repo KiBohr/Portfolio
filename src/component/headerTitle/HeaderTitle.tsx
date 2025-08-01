2eb4d36f1a8cc91ba21094a3a0b539989bdac28d
KiBohr <katharinabohr@web.de>
2025-08-01 13:28:37 +0200

    headertitle updated

--- src/component/headerTitle/HeaderTitle.tsx
+++ src/component/headerTitle/HeaderTitle.tsx
@@ -4,15 +4,20 @@ interface HeaderTitleProps {
     src: string,
     alt: string,
     className: string,
+    desc?: string,
 }
 
-const HeaderTitle = ({src, alt, className} : HeaderTitleProps) => {
-    return ( 
-        <motion.img
-        src = {src}
-        alt = {alt}
-        className = {className}>
-        </motion.img>
+const HeaderTitle = ({src, alt, className, desc} : HeaderTitleProps) => {
+    return (
+        <div className="flex flex-col items-center justify-center ">
+            <motion.img
+                src = {src}
+                alt = {alt}
+                className = {className}>
+            </motion.img>
+            {desc && <p className="text-[0.8rem] mb-15 md:mb-10 md:text-sm">{desc}</p>}
+        </div>
+        
      );
 }
  
