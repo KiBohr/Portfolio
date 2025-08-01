e6c6054f8d7ab8cc772b9c51b071e65a7e1c37a7
KiBohr <katharinabohr@web.de>
2025-08-01 13:28:52 +0200

    about me updated

--- src/page/aboutMe/AboutMe.tsx
+++ src/page/aboutMe/AboutMe.tsx
@@ -21,25 +21,27 @@ const AboutMe = () => {
         
            
              <motion.div
-             className=" flex flex-col-reverse items-center justify-center gap-5 md:gap-20 md:flex-row md:justify-start md:text-start"
+             className="w-[80vw] flex flex-col-reverse items-center justify-center gap-5 md:gap-20 md:flex-row md:justify-start md:text-start"
              transition={{duration:0.8}}
              >
            
                 <div 
-                    className="bg-white/20 rounded-xl p-4 text-[0.7rem] flex flex-col items-center text-center md:text-start md:text-[0.8rem] lg:text-base gap-2  md:w-1/2">
-                        <p className=" text-start">Moin,
+                    className="bg-white/20 rounded-xl p-5 text-[0.7rem] flex flex-col items-center text-center md:text-start md:text-[0.8rem] lg:text-base gap-2 md:w-1/2">
+                        <p className=" text-sm md:text-lg md:text-start mb-2">Moin,
                             ich freue mich über den Besuch auf meiner Seite :)
                         </p>
                         <p>
-                            Mein Name ist Katharina, ich bin fast 30 Jahre alt und habe dieses Jahr meine Umschulung als <span >Webdeveloper*in</span> abgeschlossen. 
+                            Mein Name ist Katharina, ich werde dieses Jahr 30 und habe im Mai meine Umschulung als <span className="animate-pulse duration-1000 transition ease-in-out">Webdeveloper*in</span> abgeschlossen. 
                         </p>
                         <p>
-                        Ich bin davon überzeugt, dass jede Fähigkeit dabei unterstützen kann Probleme auf <span >individuelle</span>  Art und Weise zu lösen. Meine Erfahrung als Logopäd*in hat im ersten Moment wenig mit dem Feld des Entwickelns zu tun, hilft mir aber dabei
+                        Ich bin davon überzeugt, dass jede Fähigkeit dabei unterstützen kann Probleme auf <span className="animate-pulse duration-1000 transition ease-in-out">individuelle</span>  Art und Weise zu lösen. Meine Erfahrung als Logopäd*in hat im ersten Moment wenig mit dem Feld des Entwickelns zu tun, hilft mir aber dabei:
                         </p>
-                        <ul className="list-disc flex flex-col items-start">
-                            <li>mich produktiv selbstständig zu organisieren</li>
-                            <li>effizient und zielgerichtet in einem interdisziplinären Team zu arbeiten</li>
-                            <li>und kreativ immer unterschiedliche Probleme angepasst lösen zu können.</li>
+                        
+                        
+                        <ul className="md:text-sm list-disc flex flex-col items-start px-4">
+                            <li>mich produktiv <span className="animate-pulse duration-1000 transition ease-in-out">selbstständig</span> zu organisieren</li>
+                            <li>effizient und zielgerichtet in einem interdisziplinären <span className="animate-pulse duration-1000 transition ease-in-out">Team</span> zu arbeiten</li>
+                            <li>und kreativ immer unterschiedliche <span className="animate-pulse duration-1000 transition ease-in-out">Probleme</span> angepasst <span className="animate-pulse duration-1000 transition ease-in-out">lösen</span> zu können.</li>
                         </ul>
                 </div>
 
