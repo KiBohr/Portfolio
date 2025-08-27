1a40b97ca5708d823f89ae5bebc90f4b36379e6f
KiBohr <katharinabohr@web.de>
2025-08-27 13:32:02 +0200

    added some animation to footer

--- src/component/footer/Footer.tsx
+++ src/component/footer/Footer.tsx
@@ -1,15 +1,21 @@
 import { useLocation } from "react-router-dom";
 import LinkButton from "../linkButton/LinkButton";
+import { motion } from "framer-motion";
 
 const Footer = () => {
+
 
     const location = useLocation();
     const hideSocials = location.pathname === "/contact"
 
     return ( 
-
-        <footer className="fixed bottom-0 left-0 w-full">
-            {!hideSocials && <div className="hidden md:block">
+         <motion.footer
+         initial={{ opacity: 0, scale:0 }}
+         animate={{ opacity: 1, scale:1}}
+         transition={{ duration: 1, type: "spring" }}
+         
+            className="fixed bottom-0 left-0 w-full">
+                {!hideSocials && <div className="hidden md:block">
                 <div className="flex items-center justify-end gap-4 px-8 py-5 ">
                     <LinkButton 
                     link="https://github.com/KiBohr" 
@@ -26,13 +32,14 @@ const Footer = () => {
                     />
                 </div>
             </div>}
-            
+           
 
             <div className="flex items-center justify-center gap-3 fixed bottom-5 left-0  md:justify-start md:left-5">
                 <p className="text-[0.6rem] text-red/70  ">design & code by</p>
                 <p className="text-[0.7rem] text-green  ">K Bohr</p>
             </div>
-        </footer>
+
+            </motion.footer>
      );
 }
  
